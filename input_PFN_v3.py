d795fab7a2550960b40daa528aae85a03b0d8a8c
bvermass <basilevermassen@hotmail.com>
2019-11-10 19:45:42 +0100

    PFN_v3 input specifications

--- input_PFN_v3.py
+++ input_PFN_v3.py
@@ -4,7 +4,7 @@ root_file_name = '/user/bvermass/public/2l2q_analysis/trees/HNLtagger/final/full
 validation_fraction = 0.4
 test_fraction = 0.2
 
-number_of_threads = 1
+number_of_threads = 4
 
 test = False
 
@@ -13,7 +13,7 @@ if test:
         'hidden_layers_latent' : [2],
         'nodes_latent' : [128],
         'dropout_latent' : [0.1],
-        'latent_space' : [512],
+        'latent_space' : [256],
         'hidden_layers_output' : [2],
         'nodes_output' : [128],
         'dropout_output' : [0.1],
@@ -23,14 +23,14 @@ if test:
 
 else:
     parameter_values = {
-        'hidden_layers_latent' : [2, 3],
+        'hidden_layers_latent' : [3, 4, 5],
         'nodes_latent' : [128, 256],
-        'dropout_latent' : [0, 0.1, 0.2],
-        'latent_space' : [512],
-        'hidden_layers_output' : [2, 3],
-        'nodes_output' : [128,256],
-        'dropout_output' : [0, 0.1, 0.2],
-        'learning_rate' : [1],
+        'dropout_latent' : [0.2, 0.3, 0.5],
+        'latent_space' : [256, 512],
+        'hidden_layers_output' : [3, 4, 5],
+        'nodes_output' : [128, 256],
+        'dropout_output' : [0.2, 0.3, 0.5],
+        'learning_rate' : [0.1, 1],
         'learning_rate_decay' : [1]
     }
 
